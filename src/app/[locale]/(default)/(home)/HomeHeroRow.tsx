b4456a898a92746bab04314a17c640524e800d31
freenetcoder <zandrewboyle@gmail.com>
2024-12-04 08:49:22 +0100

    upd

--- src/app/[locale]/(default)/(home)/HomeHeroRow.tsx
+++ src/app/[locale]/(default)/(home)/HomeHeroRow.tsx
@@ -133,7 +133,7 @@ export default function HomeHeroRow() {
             {t('(home).HomeHeroRow.subtitle3')}
           </p>
           <div className="flex flex-wrap items-center justify-center gap-3">
-            <Link href={`https://pump.fun`}
+            <Link href={`https://dexscreener.com/moonshot`}
              target="_blank"
              rel="noopener noreferrer">
               <Button>{t('(home).HomeHeroRow.button')}</Button>
